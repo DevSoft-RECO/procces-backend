156027a3b382336adce12628f35feb13f68a9d22
DevSoft-RECO <ronycardonaordonez@gmail.com>
2026-03-24 15:39:02 -0600

    url de la web

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,6 @@ Route::get('/', function () {
 
 Route::get('/login', function () {
     // Si falla el JWT y Laravel intenta redirigir al "login", lo mandamos de vuelta al portal Madre
-    $frontendUrl = env('APP_URL_FRONTEND', 'http://localhost:5173');
+    $frontendUrl = env('APP_URL_FRONTEND');
     return redirect($frontendUrl . '/login?session_expired=true');
 })->name('login');
